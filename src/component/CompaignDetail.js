99afd067e50b904fb0590444984f003e83b39966
santoshrks7 <santoshganji99@gmail.com>
2023-11-13 11:56:47 +0530

    removed unused imports

--- src/component/CompaignDetail.js
+++ src/component/CompaignDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import "../scss/compaigndetail.scss";
---
+++ src/utils/reusable/storage.js
@@ -0,0 +1,19 @@
+import { mockData } from "../mockdata";
+
+export const loadFromLocalStorage = () => {
+  try {
+    const stateStore = localStorage.getItem("compaign");
+    return stateStore ? JSON.parse(stateStore) : mockData || [];
+  } catch (error) {
+    console.error("Error loading state from local storage", error);
+    return undefined;
+  }
+};
+
+export const saveToLocalStorage = (state) => {
+  try {
+    localStorage.setItem("compaign", JSON.stringify(state));
+  } catch (error) {
+    console.error("Error saving state to local storage", error);
+  }
+};
